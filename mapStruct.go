b40de90d0195ba7d3eb8e4c96ddf0ef3844648fa
Sreerag R Nandan <41136404+sreeragrnandan@users.noreply.github.com>
2021-09-05 13:58:35 +0530

    Normal Struct

--- mapStruct.go
+++ mapStruct.go
@@ -20,7 +20,30 @@ func Map() {
 	fmt.Println(val, ok)
 }
 
+func normalStruct() {
+	type Student struct {
+		RollNumber    int
+		AvgMark       int
+		Name          string
+		subjectPassed []string
+	}
+
+	aStudent := Student{
+		RollNumber: 3,
+		Name:       "Sreerag",
+		subjectPassed: []string{
+			"ALL", "Sub1", "Sub2",
+		},
+	}
+	fmt.Println(aStudent.Name)
+	fmt.Println(aStudent)
+}
+
 func mapStruct() {
 	fmt.Println("\n\nMap")
 	Map()
+
+	fmt.Println("\n\nStruct")
+	normalStruct()
+
 }
